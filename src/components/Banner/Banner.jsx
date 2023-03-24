2765e75c9e51256691b6eb3a882c45ffc8e20a17
martin-centurion <martinalejandrocenturion@gmail.com>
2023-03-24 20:50:37 -0300

    agg ubicacion

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles.css';
 import { BiBeenHere } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
 
 function Banner() {
   return (
@@ -12,7 +13,9 @@ function Banner() {
             <div className='banner__content-title'>
                 <h1>Martina Avila</h1>
                 <p>En realidad me llamo la Pooro, me gustan las abejitas, queria compartirle mí invitación para festejar mi primer añito, prometo que sera divertido y vamos a poder compartir un lindo recuerdo. Los quiero!</p>
-                <button> <span><BiBeenHere /></span> Ir a mi cumple</button>
+                <Link to="https://goo.gl/maps/FDDHMJ4jCPdmXoBB9">
+                        <button> <span><BiBeenHere /></span> Ir a mi cumple</button>
+                </Link>
             </div>
         </div>
     </div>
