c5767d1996765dd81ea65de47985a33dd343859b
martin-centurion <martinalejandrocenturion@gmail.com>
2023-03-24 21:18:34 -0300

    agregando navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './styles.css';
 import { Link } from 'react-router-dom';
 import { CgBee } from 'react-icons/cg';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 function NavBar() {
 
+    const navRef = useRef();
+
+    const showNavBar = () => {
+        navRef.current.classList.toggle('responsive_nav');
+    }
 
   return (
-    <header className='header'>
-            <div className='header__content'>
-                <h1>{ <CgBee/> }</h1>
-                <nav>
-                    <Link to='/'>
-                            <button>Inicio</button>
-                    </Link>
-                    
-                    <Link to='/fotos'>
-                            <button>Mis fotitos</button>
-                    </Link>
-                </nav>
+    <div className='header'>
+        <div className='header__content container'>
+            <div className='header__content-logo'>
+                <Link to='/'>
+                    <h1><CgBee /></h1>
+                </Link>
             </div>
-    </header>
+            <div className='header__content-menu'>
+                <nav ref={navRef}>
+                    <Link to='/'>
+                        <button onClick={ showNavBar }>Home</button>
+                    </Link>
+                    <Link to='/fotos'>
+                        <button onClick={ showNavBar }>Mis Fotitos</button>
+                    </Link>
+                    <button className='nav-btn nav-close-btn' onClick={showNavBar}>
+                        <FaTimes />
+                    </button>
+                </nav>
+                <button className='nav-btn' onClick={showNavBar}>
+                    <FaBars />
+                </button>
+            </div>
+        </div>
+    </div>
   )
 }
 
